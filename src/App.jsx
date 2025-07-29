e8800473d741ec18ef06c65d3e1451d87c176bcd
Rupesh Thapa <rupeshthapayt786@gmail.com>
2025-07-29 23:11:44 +0545

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/create' element={<Create />}/>
-          <Route path='edit/blogs/:id' element={<Edit/>}/>
+          <Route path='/edit/blogs/:id' element={<Edit/>}/>
           <Route path='/single/:id' element={<Single/>}/>
           <Route path='/usestate' element={<LearnUseState />}/>
 
